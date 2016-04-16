68adbfe174e1e5f2565a391cc73e942c012c6b0f
Aditya Jha <batu.aditya007@gmail.com>
2016-04-17 03:26:24 +0530

    some state which is not complete

--- urlHandlers/productpage.py
+++ urlHandlers/productpage.py
@@ -1,5 +1,33 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 
+from scripts import utils
+
 def index(request, category_slug, product_slug):
-    return HttpResponse(product_slug)
+    try:
+        productID = utils.getIDFromSlug(product_slug)
+    except Exception as e:
+        raise Http404()
+    variables = {
+        "page_title": "homepage",
+        "sidebar_navigation": {
+            "display": True,
+            "data": [
+                {
+                    "displayName": "Kurti",
+                    "url": "kurti"
+                },
+                {
+                    "displayName": "Kurti",
+                    "url": "kurti"
+                },
+                {
+                    "displayName": "Kurti",
+                    "url": "kurti"
+                }
+            ]
+        },
+        "categories": [1,2,3,4]
+    }
+
+    return render(request, 'productpage.html', variables)
